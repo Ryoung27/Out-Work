918640cac62d13f647c1042e39b3dfc714e4ecfd
Ryoung27 <RRYoung89@gmail.com>
2020-06-27 11:18:29 -0500

    works with random numbers and timing

--- CLRS/insertion_sort.py
+++ CLRS/insertion_sort.py
@@ -1,5 +1,6 @@
 # Implmenting Insertion Sort in Python.
-
+import numpy as np
+import time
 # Uses: Insertion sort is used when number of elements is small. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
 
 # Sort an array of Size n.
@@ -8,6 +9,7 @@
 
 # Function to do insertion sort
 def insertionSort(arr):
+    start = time.perf_counter()
     # Traverse through 1 to len(arr)
     # for i in range(1, len(arr)) starts at 1 and does a count of the array.
     # Ex: arr = [100, 99, 98, 97, 96, 95] returns 1, 2, 3, 4, 5
@@ -23,8 +25,11 @@ def insertionSort(arr):
             arr[j + 1] = arr[j]
             j -= 1
             arr[j + 1] = key
+    stop = time.perf_counter()
+    print(f"Insertion sort with range 20000 took {stop - start:0.4f} seconds")
     return arr
 
-arr = [100, 99, 98, 97, 96, 95]
+arr = [np.random.randint(1,1000) for _ in range(50000)]
 
-print(insertionSort(arr))
+insertionSort(arr)
+# print(insertionSort(arr))
