6ae355d0e77f13a2eab64bf57c4cc5cab57b6594
Ryoung27 <RRYoung89@gmail.com>
2020-11-19 10:56:15 -0600

    on to the basics

--- Angular/my-first-app/src/app/server/server.component.ts
+++ Angular/my-first-app/src/app/server/server.component.ts
@@ -2,15 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
   })
 
 export class serverComponent{
     serverID: number = 10;
     serverStatus: string = 'Offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus(){
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus == 'online' ? 'green' : 'red';
     }
 
 }
--- Angular/my-first-app/src/app/servers/servers.component.ts
+++ Angular/my-first-app/src/app/servers/servers.component.ts
@@ -16,6 +16,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';
   serverName = 'Test Server';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -23,6 +25,8 @@ export class ServersComponent implements OnInit {
     }, 2000);
   }
   onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
   }
 
